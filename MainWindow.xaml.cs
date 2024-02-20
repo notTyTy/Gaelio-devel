6fcfee9d9cec404e93eee6c3e3d0fac2284b3a41
Ash <ashy@tyty.cloud>
2024-02-20 11:34:02 +0800

    changed two public methods to private

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,15 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Serialization;
 using Galileo6;
 
 // 4.1 Create two data structures using the LinkedList<T>. Must be type double
